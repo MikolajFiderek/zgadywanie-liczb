5dfe5096e91c7c9a738b285846d703bee04c5e21
MikolajFiderek <250955@edu.p.lodz.pl>
2024-06-20 22:26:04 +0200

    poprawiono bledy i dodano komentarze pomocnicze

--- main.cpp
+++ main.cpp
@@ -4,28 +4,31 @@
 using namespace std;
 
 int main() {
-
+    // losuje liczbe od 1 do 100
     int liczbaDoZgadniecia = rand() % 100 + 1;
 
-    int zgadniecia = 0;
-    int proba;
-    bool zgadniete = false;
+    int zgadniecia = 0; //inicjalizuje licznik prób na 0
+    int liczba; // inicjalizacja liczb którą podajemy
+    bool zgadniete = false; //przydatne do pętli (jesli false to zgadujesz dalej)
 
     cout << "Witaj w grze w zgadywanie liczby!\n";
     cout << "Komputer wylosowal liczbe z zakresu od 1 do 100.\n";
 
+    //pętla z negacją (jeśli nie zgadnięte)
     while (!zgadniete) {
         cout << "Podaj swoja liczbe: ";
-        cin >> proba;
-        zgadniecia++;
+        cin >> liczba;
+        zgadniecia++;//inkrementacja zwiększa licznik prób
 
-        if (proba == liczbaDoZgadniecia) {
+        //sprawdza czy liczba jest mniejsza większa lub prawidłowa
+        if (liczba == liczbaDoZgadniecia) {
             zgadniete = true;
-        } else if (proba < liczbaDoZgadniecia) {
-            cout << "To za malo. Sprobuj ponownie.\n";
+        } else if (liczba < liczbaDoZgadniecia) {
+            cout << "liczba jest za mala. Sprobuj ponownie.\n";
         } else {
-            cout << "To za duzo. Sprobuj ponownie.\n";
+            cout << "liczba jest za duza. Sprobuj ponownie.\n";
         }
+
     }
 
     cout << "Gratulacje! Zgadles liczbe " << liczbaDoZgadniecia << " po " << zgadniecia << " probach.\n";
